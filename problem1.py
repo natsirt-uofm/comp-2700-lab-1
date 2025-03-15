aa3f3a9cb80910df91ea41eda2d0e15b1d2bb29f
john bass <johntbass00@gmail.com>
2025-03-15 10:29:35 -0500

    added missing part ability

--- problem1.py
+++ problem1.py
@@ -1,9 +1,7 @@
 def is_multisubset(a,b):
      try:
           for key in a:
-               if key not in b:
-                    return False
-               if a[key] > b[key]:
+               if key not in b or a[key] > b[key]:
                     return False
           return True
      except KeyError:
@@ -24,6 +22,20 @@ def read_parts_list(filename):
      except ValueError:
           print(f"Invalid data format in {line}")
      return data
+
+def get_missing_parts(goal, inventory):
+     parts_needed = {}
+     if is_multisubset(goal, inventory) is True:
+          return parts_needed
+     for part in goal:
+          required_qty = goal[part]
+          available_qty = inventory.get(part, 0)
+          if available_qty < required_qty:
+               parts_needed[part] = required_qty - available_qty
+     return parts_needed
+
+
+          
 
 
 print(read_parts_list('collection1.txt'))
